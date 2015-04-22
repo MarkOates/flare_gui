6f216c2a4587665cb7d615d489aa3f628b85e16e
Mark Oates <euph_boy@hotmail.com>
2015-04-22 18:14:59 -0400

    updated widget_list.h with new argument orders

--- docs/widget_list.h
+++ docs/widget_list.h
@@ -8,35 +8,31 @@
 
 FGUICheckbox(FGUIParent *parent, float x, float y, float size);
 
-FGUIButton(FGUIParent *parent, std::string text, float x, float y, float w, float h);
+FGUIButton(FGUIParent *parent, float x, float y, float w, float h, std::string text);
 
 FGUIFramedWindow(FGUIParent *parent, float x, float y, float w, float h);
 
-FGUIImage(FGUIParent *parent, ALLEGRO_BITMAP *bitmap, float x, float y);
+FGUIImage(FGUIParent *parent, float x, float y, ALLEGRO_BITMAP *bitmap);
 
 FGUIMusicNotation(FGUIParent *parent, float x, float y);
 
-FGUINotificationBubble(FGUIParent *parent, std::string text, float x=300, float y=200)
+FGUINotificationBubble(FGUIParent *parent, std::string text, float x, float y)/////
 
-// this might be (val, x, y, w, h)
-FGUIProgressBar(FGUIParent *parent, float x, float y, float w=300, float h=26, float val=0.6);
+FGUIProgressBar(FGUIParent *parent, float x, float y, float w, float h, float val);
 
-// this might be (font_filename, font_size, text, x, y)
 FGUIScaledText(FGUIParent *parent, float x, float y, std::string text);
 
-// might be (content_parent, x, y, w, h)
 FGUIScrollView(FGUIParent *parent, float x, float y, float w, float h, FGUIParent *content_parent);
 
 FGUIVerticalSlider(FGUIParent *parent, float x, float y, float w, float h);
 
-// might be (text, x, y)
 FGUIText(FGUIParent *parent, float x, float y, std::string text);
 
-FGUITextArea(FGUIParent *parent, std::string text, float x, float y, float w, float h);
+FGUITextArea(FGUIParent *parent, std::string text, float x, float y, float w, float h);/////
 
-FGUITextBox(FGUIParent *parent, std::string text, float x, float y, float width, float height);
+FGUITextBox(FGUIParent *parent, std::string text, float x, float y, float width, float height);/////
 
-explicit FGUITextInput(FGUIParent *parent, std::string initial_text, float x, float y, float w, float h);
+FGUITextInput(FGUIParent *parent, std::string initial_text, float x, float y, float w, float h);/////
 
 FGUITextList(FGUIParent *parent, float x, float y, float w);
 
