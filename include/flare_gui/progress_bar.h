2c5e06d02a6ef6bbdf4e4ad74c6acabec2b81855
Mark Oates <euph_boy@hotmail.com>
2015-04-22 16:15:49 -0400

    removed default arguments for FGUIProgressBar

--- include/flare_gui/progress_bar.h
+++ include/flare_gui/progress_bar.h
@@ -14,7 +14,7 @@ private:
 	ALLEGRO_COLOR normal_color;
 	ALLEGRO_COLOR update_color;
 public:
-	FGUIProgressBar(FGUIParent *parent, float x, float y, float w=300, float h=26, float val=0.6);
+	FGUIProgressBar(FGUIParent *parent, float x, float y, float w, float h, float val);
 	void set_val(float normalized_val, float update_speed=0.5);
 	void set_val(float _val, float min, float max, float update_speed=0.5);
 	void on_draw() override;
