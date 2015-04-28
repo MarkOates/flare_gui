386fbda342b77d719e9e8ce75571f69c45575816
Mark Oates <euph_boy@hotmail.com>
2015-04-28 10:54:27 -0400

    added set_color() to FGUICheckbox

--- include/flare_gui/checkbox.h
+++ include/flare_gui/checkbox.h
@@ -13,18 +13,20 @@ class FGUICheckbox : public FGUIWidget
 private:
 	bool checked;
 	placement2d check_placement;
+	ALLEGRO_COLOR check_color;
 	float check_opacity;
+
 public:
 	FGUICheckbox(FGUIWidget *parent, float x, float y, float size);
 
-	// used
-	void on_mouse_down();
-	void on_key_char();
-	void on_joy_down();
-	void on_draw();
-
 	void toggle();
 	bool is_checked();
+	void set_color(ALLEGRO_COLOR color);
+
+	void on_mouse_down() override;
+	void on_key_char() override;
+	void on_joy_down() override;
+	void on_draw() override;
 };
 
 
--- source/checkbox.cpp
+++ source/checkbox.cpp
@@ -20,6 +20,7 @@ FGUICheckbox::FGUICheckbox(FGUIWidget *parent, float x, float y, float size)
 	: FGUIWidget(parent, new FGUICollisionBox(x, y, size, size))
 	, checked(false)
 	, check_placement(0, 0, size, size, -0.4, 0.0, 0.0, 0.45, 0.75)
+	, check_color(color::color(color::mix(color::deepskyblue, color::cyan, 0.9)))
 	, check_opacity(0)
 {
 	attr.set(FGUI_ATTR__FGUI_WIDGET_TYPE, "FGUICheckbox");
@@ -35,13 +36,17 @@ bool FGUICheckbox::is_checked()
 
 
 
+void FGUICheckbox::set_color(ALLEGRO_COLOR col)
+{
+	check_color = col;
+}
+
+
+
 void FGUICheckbox::toggle()
 {
 	checked = !checked;
 
-	// animate the check
-	FGUIScreen &super_parent = *static_cast<FGUIScreen *>(gimmie_super_parent());
-	
 	float speed = 0.2;
 	if (checked)
 	{
@@ -71,7 +76,7 @@ void FGUICheckbox::on_mouse_down()
 }
 
 
-#include <allegro_flare/allegro_flare.h>
+
 void FGUICheckbox::on_key_char()
 {
 	if (!focused) return;
@@ -81,6 +86,7 @@ void FGUICheckbox::on_key_char()
 		toggle();
 	}
 }
+
 
 
 void FGUICheckbox::on_joy_down()
@@ -94,13 +100,10 @@ void FGUICheckbox::on_joy_down()
 }
 
 
+
 void FGUICheckbox::on_draw()
 {
 	float check_inset = place.size.x*0.2;
-	ALLEGRO_COLOR fill_color = color::orange;
-	ALLEGRO_COLOR check_color = color::color(color::mix(color::deepskyblue, color::cyan, 0.9), check_opacity); // or color::greenyellow
-	FGUIScreen *super_parent = static_cast<FGUIScreen *>(gimmie_super_parent());
-	//ALLEGRO_BITMAP *gradient = super_parent->bitmaps["simple_gradient_100-01.png"];
 	ALLEGRO_BITMAP *gradient = af::bitmaps["shade_down.png"];
 	
 
@@ -108,44 +111,35 @@ void FGUICheckbox::on_draw()
 	al_draw_filled_rounded_rectangle(0, 0, place.size.x, place.size.y, 3, 3, color::color(color::black, 0.2));
 	al_draw_rounded_rectangle(1, 1, place.size.x-1, place.size.y-1, 3, 3, color::color(color::black, 0.2), 2.0);
 	al_draw_line(2, place.size.y, place.size.x-2, place.size.y, color::color(color::white, 0.2), 1.0);
-	//al_draw_rounded_rectangle(0, 0, placement.size.x, placement.size.y, 3, 3, color::color(color::white, 0.6), 2.0);
+
 
 	// draw the gradient
 	if (gradient)
 		al_draw_tinted_scaled_bitmap(gradient, color::color(color::white, 1.0), 0, 0, al_get_bitmap_width(gradient), al_get_bitmap_height(gradient),
 			check_inset, check_inset, place.size.x-check_inset*2, place.size.y-check_inset*2, ALLEGRO_FLIP_VERTICAL);
 
+
 	// draw the check
-	//if (checked)
-	//{
-		ALLEGRO_FONT *font_awesome = af::fonts["FontAwesome.otf " + tostring(((int)(place.size.x*1.75)))];
+	ALLEGRO_FONT *font_awesome = af::fonts["FontAwesome.otf " + tostring(((int)(place.size.x*1.75)))];
 
-		static ALLEGRO_USTR *ustr = al_ustr_new("");
-		//if (!ustr) ustr = ;
-		al_ustr_set_chr(ustr, 0, font_awesome::ok);
-		check_placement.position.x = place.size.x/2 + place.size.x*0.1;
-		check_placement.position.y = place.size.y/2 + place.size.y*0.3;
-		check_placement.size.x = al_get_ustr_width(font_awesome, ustr);
-		check_placement.size.y = al_get_font_line_height(font_awesome);
-		
-		if (checked)
-		{
-			float boxfade_opacity = 1.0 - check_opacity;
-			al_draw_filled_rectangle(check_inset, check_inset, place.size.x-check_inset, place.size.y-check_inset, color::color(check_color, boxfade_opacity));
-		}
-		//al_draw_rectangle(check_inset, check_inset, placement.size.x-check_inset, placement.size.y-check_inset, color::mix(color::black, fill_color, 0.9), 2.0);
-//		float check_x = placement.size.x/2+placement.size.x*0.1;
-//		float check_y = placement.size.y/2 - al_get_font_line_height(font_awesome)/2 - placement.size.x*0.2;
-		check_placement.start_transform();
+	static ALLEGRO_USTR *ustr = al_ustr_new("");
+	al_ustr_set_chr(ustr, 0, font_awesome::ok);
+	check_placement.position.x = place.size.x/2 + place.size.x*0.1;
+	check_placement.position.y = place.size.y/2 + place.size.y*0.3;
+	check_placement.size.x = al_get_ustr_width(font_awesome, ustr);
+	check_placement.size.y = al_get_font_line_height(font_awesome);
+	
+	if (checked)
+	{
+		float boxfade_opacity = 1.0 - check_opacity;
+		al_draw_filled_rectangle(check_inset, check_inset, place.size.x-check_inset, place.size.y-check_inset, color::color(check_color, boxfade_opacity));
+	}
+	check_placement.start_transform();
 
-		//faicon::draw(font_awesome, color::color(color::black, 0.3), faicon::ok, );
-		//faicon::draw(font_awesome, check_color, faicon::ok, ALLEGRO_ALIGN_CENTRE, check_x, check_y);
+	al_draw_ustr(font_awesome, color::color(color::black, 0.3), 0, 3, NULL, ustr);
+	al_draw_ustr(font_awesome, check_color, 0, 0, NULL, ustr);
 
-		al_draw_ustr(font_awesome, color::color(color::black, 0.3), 0, 3, NULL, ustr);
-		al_draw_ustr(font_awesome, check_color, 0, 0, NULL, ustr);
-
-		check_placement.restore_transform();
-	//}
+	check_placement.restore_transform();
 }
 
 
