106599d4be0f300bf09421da4791d7a3d7c56e12
Mark Oates <euph_boy@hotmail.com>
2015-08-04 04:44:55 -0400

    Include spinners in flare_gui.h

--- include/flare_gui/flare_gui.h
+++ include/flare_gui/flare_gui.h
@@ -21,6 +21,9 @@
 #include <flare_gui/widgets/text.h>
 #include <flare_gui/widgets/text_input.h>
 #include <flare_gui/widgets/text_box.h>
+#include <flare_gui/widgets/spinner.h>
+#include <flare_gui/widgets/int_spinner.h>
+#include <flare_gui/widgets/list_spinner.h>
 #include <flare_gui/widgets/scaled_text.h>
 #include <flare_gui/widgets/text_area.h>
 #include <flare_gui/widgets/checkbox.h>
