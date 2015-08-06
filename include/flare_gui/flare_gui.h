2a59a4b0402e162fbeee8059e3bc0d523020b223
Mark Oates <euph_boy@hotmail.com>
2015-08-06 02:00:47 -0400

    Alphabetize list of include files

--- include/flare_gui/flare_gui.h
+++ include/flare_gui/flare_gui.h
@@ -18,25 +18,27 @@
 #include <flare_gui/style_assets.h>
 
 #include <flare_gui/widgets/button.h>
-#include <flare_gui/widgets/text.h>
-#include <flare_gui/widgets/text_input.h>
-#include <flare_gui/widgets/text_box.h>
-#include <flare_gui/widgets/spinner.h>
-#include <flare_gui/widgets/int_spinner.h>
-#include <flare_gui/widgets/list_spinner.h>
-#include <flare_gui/widgets/scaled_text.h>
-#include <flare_gui/widgets/text_area.h>
 #include <flare_gui/widgets/checkbox.h>
 #include <flare_gui/widgets/dial.h>
-#include <flare_gui/widgets/toggle_button.h>
-#include <flare_gui/widgets/slider.h>
-#include <flare_gui/widgets/scrollbar.h>
-#include <flare_gui/widgets/image.h>
-#include <flare_gui/widgets/text_list.h>
-#include <flare_gui/widgets/window.h>
-#include <flare_gui/widgets/framed_window.h>
-#include <flare_gui/widgets/scroll_area.h>
 #include <flare_gui/widgets/draggable_region.h>
+#include <flare_gui/widgets/framed_window.h>
+#include <flare_gui/widgets/image.h>
+#include <flare_gui/widgets/int_spinner.h>
+#include <flare_gui/widgets/list_spinner.h>
+#include <flare_gui/widgets/music_notation.h>
+#include <flare_gui/widgets/progress_bar.h>
+#include <flare_gui/widgets/scaled_text.h>
+#include <flare_gui/widgets/scroll_area.h>
+#include <flare_gui/widgets/scrollbar.h>
+#include <flare_gui/widgets/slider.h>
+#include <flare_gui/widgets/spinner.h>
+#include <flare_gui/widgets/text.h>
+#include <flare_gui/widgets/text_area.h>
+#include <flare_gui/widgets/text_box.h>
+#include <flare_gui/widgets/text_input.h>
+#include <flare_gui/widgets/text_list.h>
+#include <flare_gui/widgets/toggle_button.h>
+#include <flare_gui/widgets/window.h>
 #include <flare_gui/widgets/xy_controller.h>
 
 
